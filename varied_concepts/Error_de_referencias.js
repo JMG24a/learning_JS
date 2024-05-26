b3d9a8ca133992a031a8e5dc14f7519f5bf5fad2
jmg24a <jmg24a@gmail.com>
2024-05-25 22:46:32 -0400

    [fix]

--- varied_concepts/Error_de_referencias.js
+++ varied_concepts/Error_de_referencias.js
@@ -1,3 +1,4 @@
+// example 1
 /*
     Por que ReferenceError?
     Las variables declaradas con let y const tienen scope de bloque es por este motivo que si bien tenemos 2 variables con el nombre name, ambas son diferentes e independientes en sus respectivos scopes.
@@ -15,7 +16,6 @@
 
     Por hoisting el resultado sería undefined puesto que la Temporal Dead Zone solo existe con variables declaradas con let y const.
 */
-
 const name = 'a'
 
 function getName () {
@@ -25,3 +25,21 @@ function getName () {
 }
 
 getName()
+
+
+// example 2
+/*
+ReferenceError: Cannot access "name" before initialization:
+
+Primero, recordar que las variables declaradas con let o const tienen scope de bloque.
+Segundo, recordar que las variables declaradas con let o const no tienen hoisting, cuando intentamos acceder a una variable antes de su inicializción entra en la Temporal Dead Zone. La variable name no puede ser mostrada sin antes inicializarla, name esta en su Temporal Dead Zone.
+*/
+
+function test() {
+  let name = 'Alex';
+  if (true) {
+    console.log(name);
+    let name = 'Oscar';
+  }
+}
+console.log(test());
--- web_workers/index.js
+++ web_workers/index.js
@@ -1,26 +1,26 @@
 /* -----------------------------------Without Optimization--------------------------------------------- */
-const volume = document.querySelector('#volume') 
-const volumeResult = document.querySelector('#volumeResult')
-// fibonacci
-const fibNumber = document.querySelector('#fibNumber')
-const fibButton = document.querySelector('#fibButton')
-const fibResult = document.querySelector('#fibResult')
+// const volume = document.querySelector('#volume') 
+// const volumeResult = document.querySelector('#volumeResult')
+// // fibonacci
+// const fibNumber = document.querySelector('#fibNumber')
+// const fibButton = document.querySelector('#fibButton')
+// const fibResult = document.querySelector('#fibResult')
 
-fibButton.addEventListener('click', () => {
-    const result = fib(fibNumber.value);
-    fibResult.innerHTML = `<li>${result}</li>`
-})
+// fibButton.addEventListener('click', () => {
+//     const result = fib(fibNumber.value);
+//     fibResult.innerHTML = `<li>${result}</li>`
+// })
 
-volume.addEventListener('input', (event)=>{
-    volumeResult.innerHTML = `${event.target.value}%`;
-})
+// volume.addEventListener('input', (event)=>{
+//     volumeResult.innerHTML = `${event.target.value}%`;
+// })
 
-function fib(n){
-    if(n < 2){
-        return n;
-    }
-    return fib(n-1) + fib(n-2)
-}
+// function fib(n){
+//     if(n < 2){
+//         return n;
+//     }
+//     return fib(n-1) + fib(n-2)
+// }
 
 /* -----------------------------------Optimization With Web Worker--------------------------------------------- */
 // const worker = new window.Worker('wk_fib.js') // Init constructor of the worker 
@@ -45,32 +45,31 @@ function fib(n){
 //     volumeResult.innerHTML = `${event.target.value}%`;
 // })
 
+/* -----------------------------------Optimization With Memorization--------------------------------------*/
+const fibResults = document.querySelector('#fibResult');
+const fibButton = document.querySelector('#fibButton')
+const fibNumber = document.querySelector('#fibNumber')
 
-// // Objeto para almacenar resultados previamente calculados
-// const fibResults = document.querySelector('#fibResult');
-// const fibButton = document.querySelector('#fibButton')
-// const fibNumber = document.querySelector('#fibNumber')
+fibButton.addEventListener('click', () => {
+    const result = fib(fibNumber.value);
+    fibResults.innerHTML = `<li>${result}</li>`
+})
 
-// fibButton.addEventListener('click', () => {
-//     const result = fib(fibNumber.value);
-//     fibResults.innerHTML = `<li>${result}</li>`
-// })
+function fib(n) {
+  // Verify result has been previously calculated
+  if (fibResults.hasOwnProperty(n)) {
+    return fibResults[n];
+  }
 
-// function fib(n) {
-//   // Verificar si el resultado ya ha sido calculado previamente
-//   if (fibResults.hasOwnProperty(n)) {
-//     return fibResults[n];
-//   }
+  // calculate result if did not save
+  let result;
+  if (n < 2) {
+    result = n;
+  } else {
+    result = fib(n - 1) + fib(n - 2);
+  }
 
-//   // Calcular el resultado solo si no está almacenado previamente
-//   let result;
-//   if (n < 2) {
-//     result = n;
-//   } else {
-//     result = fib(n - 1) + fib(n - 2);
-//   }
-
-//   // Almacenar el resultado para uso futuro
-//   fibResults[n] = result;
-//   return result;
-// }
+  // save result
+  fibResults[n] = result;
+  return result;
+}
